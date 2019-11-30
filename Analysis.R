729210d2ecc886d9bde08fb04342525430a5a530
airdipu <48720548+airdipu@users.noreply.github.com>
2019-11-30 19:17:18 +0100

    Update Analysis.R
    Modified code

--- Analysis.R
+++ Analysis.R
@@ -1,41 +1,42 @@
-&gt; install.packages(&quot;meta&quot;)
-&gt; library(meta)
-&gt; dat&lt;-read.csv(&quot;K:\\Cancer Research\\Meta Anik\\new paper\\Cancer.csv&quot;, header =TRUE)
-&gt; str(dat)
+> install.packages("meta")
+> library(meta)
+> dat<-read.csv("K:\\Cancer.csv", header =TRUE)
+> str(dat)
 
-###Fitting the fixed effect model
-Y &lt;- with(dat, log(Gender*Area*p/(n*N)))
-V &lt;- with(dat, 1/Area + 1/p + 1/n + 1/N)
-&gt; cbind(Y,V)
-&gt; result.or &lt;- rma(yi = Y, vi = V, method = &quot;FE&quot;)
-&gt; summary(result.or)
+### Fitting the fixed effect model
 
-###Confidence Interval
+Y <- with(dat, log(Gender*Area*p/(n*N)))
+V <- with(dat, 1/Area + 1/p + 1/n + 1/N)
+> cbind(Y,V)
+> result.or <- rma(yi = Y, vi = V, method = "FE")
+> summary(result.or)
 
-&gt; confint(result.or)
+### Confidence Interval
 
-###Fitting Random Effects Model
+> confint(result.or)
 
-&gt; re &lt;- rma(yi = Y, vi = V, data=dat)
-&gt; summary(re)
+### Fitting Random Effects Model
 
-###Forest plotting
+> re <- rma(yi = Y, vi = V, data=dat)
+> summary(re)
 
-&gt; forest(re, slab = paste(dat$Author, as.character(dat$Year), sep = &quot;, &quot;))
+### Forest plotting
 
-###Funnel Plotting. A common way to investigate potential publication bias in a meta-analysis is the
-funnel plot.
-Asymmetrical distribution indicates potential publication bias.
+> forest(re, slab = paste(dat$Author, as.character(dat$Year), sep = ", "))
 
-&gt; funnel(re)
+### Funnel Plotting. A common way to investigate potential publication bias in a meta-analysis is the funnel plot. 
+### Asymmetrical distribution indicates potential publication bias.
 
-###Differences measure
+> funnel(re)
 
-&gt; boxplot(Y ~ Area, data = dat)
+### Differences measure
 
-###Mesuring tau2 for different method
-&gt; estimators &lt;- c(&quot;DL&quot;, &quot;REML&quot;, &quot;HE&quot;, &quot;HS&quot;, &quot;SJ&quot;, &quot;ML&quot;, &quot;EB&quot;)
-&gt; taus &lt;- sapply(estimators, function(method){re$tau2})
-&gt; plot(y = taus, x = 1:length(taus),type = &quot;h&quot;, pch = 19,axes = FALSE, xlab = &quot;Estimators&quot;)
-&gt; axis(2, las = 1)
-&gt; axis(1, at = 1:length(taus), lab = estimators)
+> boxplot(Y ~ Area, data = dat)
+
+### Mesuring tau2 for different method
+
+> estimators <- c("DL", "REML", "HE", "HS", "SJ", "ML", "EB")
+> taus <- sapply(estimators, function(method){re$tau2})
+> plot(y = taus, x = 1:length(taus),type = "h", pch = 19,axes = FALSE, xlab = "Estimators")
+> axis(2, las = 1)
+> axis(1, at = 1:length(taus), lab = estimators)
